8b078eb8b977d4ef831c0a666ed8789865e7f1c7
Hugh Isaacs II <HughIsaacs2@users.noreply.github.com>
2019-03-30 15:13:02 -0400

    Last upload before new release
    + Got more specific with the selector for changing dat:// links in datsite.js.

--- chrome extension/datsite.js
+++ chrome extension/datsite.js
@@ -1,1 +1,6 @@
 var DatPart = true;
+
+Array.from(document.querySelectorAll('a[href^="dat://"]:not(a[href*="."])')).forEach(function(link){
+    var theLink = link.href;
+	link.href = "web+" + theLink;
+});
